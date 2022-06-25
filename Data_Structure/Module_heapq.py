2608756b772c12d3f0272e0e5593ab69430d5c04
zzz <099025zzz@gmail.com>
2022-06-25 23:44:12 +0300

    Learn module heapq is done!
    Signed-off-by: zzz <099025zzz@gmail.com>

--- Data_Structure/Module_heapq.py
+++ Data_Structure/Module_heapq.py
@@ -1,1 +1,66 @@
 import heapq
+import random
+
+
+def heap_push():
+    data = [random.randint(1, 20) for _ in range(10)]
+    print(f"Data: {data}")
+    heap = []
+
+    for element in data:
+        heapq.heappush(heap, element)
+
+    print(heap)
+
+def heap_pop():
+    data = [random.randint(1, 20) for _ in range(10)]
+    heapq.heapify(data)
+    print(f"Data: {data}")
+    print(f"HEAP POP: {heapq.heappop(data)}")
+    print(f"After POP: {data}")
+
+def heapify():
+    data = [random.randint(1, 20) for _ in range(10)]
+    print(f"Data: {data}")
+    heapq.heapify(data)
+    print(f"Heap data: {data}")
+
+def heapreplace():
+    # heapreplace = heappop + heapush
+    data = []
+    for _ in range(10):
+        heapq.heappush(data, random.randint(1, 20))
+    print(f"Data: {data}")
+    for i in range(5):
+        heapq.heapreplace(data, i)
+
+    print(f"Data after replace: {data}")
+
+def heap_small_largest():
+    data = [random.randint(1,20) for _ in range(10)]
+    heapq.heapify(data)
+    print(f"Data: {data}")
+    print(f"Smallest: {heapq.nsmallest(2, data)}")
+    print(f"Largest: {heapq.nlargest(2, data)}")
+
+def merge():
+    data = []
+    for i in range(4):
+        new_data = [random.randint(1, 30) for _ in range(10)]
+        new_data.sort() # ONLY sorted lists
+        data.append(new_data)
+
+    for i, d in enumerate(data):
+        print(f"{i}: {d}")
+
+    for d in heapq.merge(*data):
+        print(f"{d} ", end="")
+
+
+# heap_push()
+# heap_pop()
+# heapify()
+# heapreplace()
+# heap_small_largest()
+# merge()
+
